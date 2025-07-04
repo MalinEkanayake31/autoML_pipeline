9c045c91ce7a5b703a6d87a67939a6cf5959d27a
Malin Ashmitha Ekanayake <159604336+MalinEkanayake31@users.noreply.github.com>
2025-07-04 15:42:25 +0530

    Model selection upto steps all updates

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, field_validator
 import os
 
 class Config(BaseModel):
@@ -6,13 +6,15 @@ class Config(BaseModel):
     target_column: str
     task_type: str
 
-    @validator("file_path")
+    @field_validator("file_path")
+    @classmethod
     def check_file_exists(cls, v):
         if not os.path.exists(v):
             raise ValueError("Provided file path does not exist")
         return v
 
-    @validator("task_type")
+    @field_validator("task_type")
+    @classmethod
     def validate_task(cls, v):
         if v not in ["classification", "regression", "clustering"]:
             raise ValueError("Task type must be classification, regression, or clustering")
